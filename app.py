5d610bfeefef0fcddbb225855b6d3973c9e94234
Sankalpgupta74 <sankalp.gupta_cs.aiml23@gla.ac.in>
2026-04-28 10:10:23 +0530

    Update app.py

--- app.py
+++ app.py
@@ -20,5 +20,4 @@ def predict():
     return render_template("index.html", prediction_text=f"Prediction: {prediction[0]}")
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port)
+    app.run(host="0.0.0.0", port=8000)
